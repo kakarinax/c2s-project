eda97e77fa35bd9aa1d0316aa3615d8b547db356
Karina <s.karinaalmeida@gmail.com>
2023-04-07 14:56:32 -0300

    add uploader using carrier wave gem

---
+++ app/uploaders/email_uploader.rb
@@ -0,0 +1,12 @@
+class EmailUploader < CarrierWave::Uploader::Base
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_allowlist
+    %w[eml]
+  end
+end
