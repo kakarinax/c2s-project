b945f46472dcd820771aa7d882a2099e06cf9f9f
Karina <s.karinaalmeida@gmail.com>
2023-04-07 14:55:41 -0300

    add bootstrap to make the desing part easier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,9 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 Rails.start()
 Turbolinks.start()
