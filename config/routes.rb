1677c43c9c103f93f7513b79fc0dc5cc8d157e87
Karina <s.karinaalmeida@gmail.com>
2023-04-10 00:01:53 -0300

    routes and application html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'messages#new'
   resources :messages, only: %i[new create show]
+  mount Sidekiq::Web => '/sidekiq'
 end
